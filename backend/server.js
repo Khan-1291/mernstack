4a8490c8028126cffbd3b1a8af2adc032b185a73
Zohaib Khan | Front-End Web Developer <zk3165657513@gmail.com>
2026-03-04 00:28:55 +0500

    Initial commit for Project 1

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+const express= require("express")
+const mongoose= require('mongoose')
+
+require('dotenv').config()
+
+const workoutRoutes= require('./routes/workouts')
+
+const app= express()
+app.use(express.json())
+app.use((req,res,next) =>{
+      
+  console.log(req.path)
+  console.log(req.method)
+
+    next()
+})
+
+app.use('/api/workouts', workoutRoutes)
+ 
+mongoose.connect(process.env.MONGO_URL).
+then(()=>{
+    app.listen(process.env.PORT,()=>{
+        console.log("Server is listining on port" + process.env.PORT)
+    })
+
+}).
+catch(
+    (error) =>{
+console.log(error)
+    }
+)
+
+
+/*app.listen(process.env.PORT ,()=>{
+    console.log("app is lisstining on prt no"+  process.env.PORT)
+})  */
